86f6be4854c2f9743435173ee58bb8ce316604c4
Adam Marko <adko87@hotmail.com>
2024-10-05 18:18:52 -0600

    add function to update voice_lines.json

--- Voiceline Generator/generate_voicelines.py
+++ Voiceline Generator/generate_voicelines.py
@@ -2,6 +2,8 @@ import requests  # Used for making HTTP requests
 import json  # Used for working with JSON data
 import os  # Used for interacting with the operating system
 
+print("App started")
+
 # Get Eleven Labs API key from config.json
 with open('../config.json') as config_file:
     config = json.load(config_file)
@@ -12,15 +14,146 @@ api_key = config['api_key']
 CONTACTS_FILE_PATH = "../Game Files/Data/contacts.json"
 VOICE_LINES_FILE_PATH = "../Game Files/Data/voice_lines.json"
 OUTPUT_FOLDER = "../Game Files/Assets/VoiceLines"
-API_KEY = api_key
+CHUNK_SIZE = 1024  
+XI_API_KEY = api_key
 
-# Get data from contacts.json
-with open(CONTACTS_FILE_PATH) as contacts_file:
-    data = json.load(contacts_file)
+def create_audio_file(api_response, file_name, folder_name):
+        # Create the directory to save the audio files in
+        os.makedirs(os.path.join(OUTPUT_FOLDER, folder_name), exist_ok=True)
 
-contacts = data['contacts']
+        file_name_and_path = os.path.join(OUTPUT_FOLDER, folder_name, file_name)
+       
+        # Open the output file in write-binary mode
+        with open(file_name_and_path, "wb") as f:
+            # Read the response in chunks and write to the mp3 audio file
+            for chunk in api_response.iter_content(chunk_size=CHUNK_SIZE):
+                f.write(chunk)
+        
+        print("Audio stream saved successfully.")
 
-print(contacts)
+def call_text_to_speech_api(voice_id, text, file_name, folder_name):
+
+    headers = {
+        "Accept": "application/json",
+        "xi-api-key": XI_API_KEY
+    }
+     
+    url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
+
+    payload = {
+        "text": text,
+        "model_id": "eleven_monolingual_v1",
+        "voice_settings": {
+        "stability": 0.5,
+        "similarity_boost": 0.8,
+        "style": 0.0,
+        "use_speaker_boost": True
+        }
+    }
+   
+    response = requests.request("POST", url, json=payload, headers=headers)
+    
+    # Check if the request was successful
+    if response.ok:
+        create_audio_file(
+            api_response=response, 
+            file_name=file_name, 
+            folder_name=folder_name
+        )
+        
+    else:
+        # Print the error message if the request was not successful
+        print(response.text)
+        
+    return response.text
+
+
+# Update voice_lines.json with the generated assets details
+def update_voice_lines_file(generated_file_name, voice_actor, line, line_index): 
+    with open(VOICE_LINES_FILE_PATH, 'r') as file:
+        data = json.load(file)
+
+        voice_line_object = data['voiceLines'][line_index]
+      
+        # Add new properties to the voice_line_object
+        # to refernce the generated assets
+        if 'generated_assets' not in voice_line_object:
+            voice_line_object['generated_assets'] = []
+
+        generated_asset_info = {
+            "line": line,
+            "audio_file_name": generated_file_name,
+            "voice_actor": voice_actor
+        }
+        # Add new properties to the file
+        voice_line_object['generated_assets'].append(generated_asset_info)
+
+        # Write the updated data back to the file
+        with open(VOICE_LINES_FILE_PATH, 'w') as updated_file:
+            json.dump(data, updated_file, indent=4)
+
+def process_voice_line_file(file_data, file_name, file_path, line_index):
+    current_voice_line = str(line_index + 1)
+    print("Processing voice line " + current_voice_line)
+    
+    lines = file_data.get('lines', [])
+    valid_speakers = file_data.get('valid_speakers', [])
+
+    if (len(valid_speakers) == 0):
+        use_random_voice_id = True
+    else:
+        use_random_voice_id = False
+
+    if len(lines) == 0:
+        print("No lines found in file")
+    else:
+        for index, line in enumerate(lines):
+            voice_id = get_voice_id_from_name("test")
+            text = line
+            # TODO: Update file and folder naming
+            audio_file_name = f"{index}_{file_name}.mp3"
+            folder_name = f"{"New"}"
+
+            call_text_to_speech_api(
+                voice_id=voice_id,
+                text=text,
+                file_name=audio_file_name,
+                folder_name=folder_name
+            ) 
+            update_voice_lines_file(
+                generated_file_name=audio_file_name,
+                voice_actor=voice_id,
+                line=line,
+                line_index=line_index
+            )
+
+    print("Finished processing voice line " + current_voice_line)
+
+# TODO: Get the voice ID from the name
+def get_voice_id_from_name(name):
+    
+    return "CwhRBWXzGAHq8TQ4Fs17"
+
+# Check if each contact has an associated voice ID
+try:
+    with open(CONTACTS_FILE_PATH) as contacts_file:
+        data = json.load(contacts_file)
+
+    contacts = data['contacts']
+
+    for contact in contacts:
+        try:
+            # Check if contact has a 'elevenLabsId' property
+            print(contact['elevenLabsId'])  
+        except KeyError:
+            # Handle the case where the property is missing
+            print("No associated voice ID found for the character",contact['name'])
+
+except FileNotFoundError:
+    print(f"Error: The file {CONTACTS_FILE_PATH} was not found.")
+except json.JSONDecodeError:
+    print("Error: Failed to decode JSON from the contacts file.")
+
 
 # Get data from voice_lines.json
 with open(VOICE_LINES_FILE_PATH) as voice_lines_file:
@@ -29,156 +162,51 @@ with open(VOICE_LINES_FILE_PATH) as voice_lines_file:
 voice_lines = data['voiceLines']
 
 # Create a temporary folder to store each voice line object as a separate JSON file
-folder = 'voice_lines_output_tmp'
-os.makedirs(folder, exist_ok=True)
+tmp_folder = 'voice_lines_output_tmp'
+os.makedirs(tmp_folder, exist_ok=True)
 
 # Write each voice line object to a separate JSON file
+# to make it easier to work with
 for index, item in enumerate(data["voiceLines"]):
-    print(index)
-    print(item)
-    file_name = f"voice_line_{index}.json"
-    file_path = os.path.join(folder, file_name)
+    
+    file_name = f"voice_line_{index + 1}.json"
+    file_path = os.path.join(tmp_folder, file_name)
 
     with open(file_path, 'w') as file:
         json.dump(item, file, indent=4)
 
-# for index, item in voice_lines:
-#     for key, value in item.items():
-#         print(f"{key}: {value}")
-    
-
-
-
-data = {
-    "name": "John",
-    "age": 30,
-    "city": "New York"
-}
-
-file_name = 'line1.json'
-file_path = os.path.join(OUTPUT_FOLDER, file_name)
-
-# Writing data to a JSON file
-with open(file_path, 'w') as file:
-    json.dump(data, file, indent=4)
-
-# # Define constants
-# CHUNK_SIZE = 1024  # Size of chunks to read/write at a time
-# XI_API_KEY = api_key  # Your API key for authentication
-# OUTPUT_FOLDER = "speech_files"
-# EXCEL_FILE_PATH = "unprocessed_files/lines.xlsx"
-
-# print("App started!")
-# print("Audio files will be saved in the 'speech_files' folder.")
-
-# def call_text_to_speech_api(voice_id, text, file_name, folder_name):
-
-#     headers = {
-#         "Accept": "application/json",
-#         "xi-api-key": XI_API_KEY
-#     }
-     
-#     url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
-
-#     payload = {
-#         "text": text,
-#         "model_id": "eleven_monolingual_v1",
-#         "voice_settings": {
-#         "stability": 0.5,
-#         "similarity_boost": 0.8,
-#         "style": 0.0,
-#         "use_speaker_boost": True
-#         }
-#     }
-   
-#     response = requests.request("POST", url, json=payload, headers=headers)
-    
-#     # Check if the request was successful
-#     if response.ok:
-
-#         # Create the directory to save the audio files in
-#         # group by voice actor name in the speech_files folder
-#         os.makedirs(os.path.join(OUTPUT_FOLDER, folder_name), exist_ok=True)
-
-#         file_name_and_path = os.path.join(OUTPUT_FOLDER, folder_name, file_name)
-       
-#         # Open the output file in write-binary mode
-#         with open(file_name_and_path, "wb") as f:
-#             # Read the response in chunks and write to the mp3 audio file
-#             for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-#                 f.write(chunk)
-#         # Inform the user of success
-#         print("Audio stream saved successfully.")
-#     else:
-#         # Print the error message if the request was not successful
-#         print(response.text)
+# Run function for each JSON file in the temporary folder
+for index, file_name in enumerate(os.listdir(tmp_folder)):
+    if file_name.endswith('.json'):
+        file_path = os.path.join(tmp_folder, file_name)
         
-#     return response.text
-
-# # Get the list of available voices from API
-# def get_voices():
-
-#     headers = {
-#         "Accept": "application/json",
-#         "xi-api-key": XI_API_KEY,
-#         "Content-Type": "application/json"
-#     }
-
-#     url = "https://api.elevenlabs.io/v1/voices"
-
-#     response = requests.request("GET", url, params={"api_key": api_key}, headers=headers)
-
-#     response_json = response.json()
-
-#     voices = response_json["voices"]
-    
-#     # Convert voices json into a DataFrame
-#     df = pd.DataFrame(voices)
-
-#     # Save the available voices returned from the api to an Excel file
-#     df.to_excel('voices.xlsx', index=False)
-    
-#     return voices
-
-# get_voices()
-
-# # Read the unprocessed Excel file to get data for the API call
-# def get_data_from_excel_file():
-
-#     # Read the unprocessed Excel file into a DataFrame
-#     df = pd.read_excel(EXCEL_FILE_PATH)
-
-#     for index, row in df.iterrows():
-#         line_id = row['line_id']
-#         voice_id = row['voice_id']
-#         text = row['text']
-#         name = row['name'].lower()
-#         file_name = f"{line_id}_{voice_id}.mp3"
-#         folder_name = f"{name}"
-#         print(f"Line ID: {line_id}, Voice ID: {voice_id}, Text: {text}")
+        # Open and read the JSON file
+        with open(file_path, 'r') as file:
+            try:
+                file_data = json.load(file)  
+                process_voice_line_file(
+                    file_data=file_data, 
+                    file_name=file_name,
+                    file_path=file_path,
+                    line_index=index
+                )  
+            except json.JSONDecodeError as err:
+                print(f"Error reading {file_name}: {err}")
         
-#         # Call the Text-to-Speech API and save the audio stream to an output file
-#         # for each row in the Excel file
+# Delete the temporary folder after processing
+try:
+    # Remove all files in the directory
+    for file_name in os.listdir(tmp_folder):
+        file_path = os.path.join(tmp_folder, file_name)
+        os.remove(file_path) 
 
-#         # TODO: Pass file name (index)
-#         # 
-#         call_text_to_speech_api(
-#             voice_id=voice_id,
-#             text=text,
-#             file_name=file_name,
-#             folder_name=folder_name
-            
-#         )    
-
-# get_data_from_excel_file()
-
-# # Move the processed files to the processed_files folder
-# def move_files():
+    # Remove the directory itself
+    os.rmdir(tmp_folder)  # Remove the empty directory
     
-#     source_path = 'unprocessed_files/lines.xlsx'
+except Exception as err:
+    print(f"Error deleting temporary folder: {err}")      
 
-#     destination_path = 'processed_files/lines.xlsx'
 
-#     os.rename(source_path, destination_path)
 
-# move_files()
+
+print("App finished. Assets generated in " + OUTPUT_FOLDER)
